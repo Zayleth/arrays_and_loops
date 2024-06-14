5e480ba27782e03e62b7aeaf1a1dc154db4b38b1
Zayleth <zaylethvegas@gmail.com>
2024-06-14 16:50:51 -0400

    Exercises (3). Arrays

---
+++ arrays.js
@@ -0,0 +1,42 @@
+// Test Yourself With Exercises
+/* Exercise 1:
+Get the value "Volvo" from the cars array.
+*/
+
+const cars = ["Saab", "Volvo", "BMW"];
+let x = cars[1];
+
+////////////////////
+
+
+
+/* Exercise 2:
+Change the first item of cars to "Ford".
+*/
+
+// const cars = ["Volvo", "Jeep", "Mercedes"];
+cars[0] = "Ford";
+
+////////////////////
+
+
+
+/* Exercise 3:
+Alert the number of items in an array, using the correct Array property.
+*/
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.length);
+
+///////////////////
+
+
+
+/* Exercise 3:
+Use the correct Array method to remove the last item of the fruits array.
+*/
+
+const fruits = ["Banana", "Orange", "Apple"];
+remove[2]
+
+// to fix
