e28a70fdd8922485a7ab84dd710a6940d8b78d57
Zayleth <zaylethvegas@gmail.com>
2024-06-17 00:08:36 -0400

    Array Methods (3 exercises)

---
+++ array_methods.js
@@ -0,0 +1,19 @@
+// Test Yourself With Exercises
+// Exercise 1:
+// Use the correct Array method to remove the last item of the fruits array.
+
+const fruits = ["Banana", "Orange", "Apple"];
+fruits.pop();
+
+// Exercise 2:
+// Use the correct Array method to add "Kiwi" to the fruits array.
+
+const fruitss = ["Banana", "Orange", "Apple"];
+fruitss.push("Kiwi")
+;
+
+// Exercise 3:
+// Use the splice() method to remove "Orange" and "Apple" from fruits.
+
+const fruitsss = ["Banana", "Orange", "Apple", "Kiwi"];
+fruitsss.splice(1, 2);
